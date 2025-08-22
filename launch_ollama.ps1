3734caa6c9081091c1870f7346a3bbca759f9455
Fedor Kudashev <fffeek@gmail.com>
2025-08-22 14:27:59 +0300

    Fixed PowerShell installation script

--- launch_ollama.ps1
+++ launch_ollama.ps1
@@ -1,10 +1,14 @@
-#Setting environmental variables
-$env:OLLAMA_ORIGINS="chrome-extension://*"
-
 #Downloading and installing Ollama
 Invoke-WebRequest -Uri "https://ollama.com/download/OllamaSetup.exe" -OutFile "OllamaSetup.exe"
 Start-Process -FilePath "OllamaSetup.exe" -ArgumentList "/S" -Wait
 
-#Downloading LLM and starting Ollama
+#Downloading LLMs
+ollama pull gemma3:270m
 ollama pull gemma3:1b
+
+#Stopping Ollama
+Get-Process | Where-Object {$_.ProcessName -like '*ollama*'} | Stop-Process
+
+#Setting environmental variables and starting Ollama
+$env:OLLAMA_ORIGINS="chrome-extension://*"
 ollama serve
