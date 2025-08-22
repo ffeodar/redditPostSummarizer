5b33ad1764c58e30ee8c048e5e8add0f7c4194b9
Fedor Kudashev <fffeek@gmail.com>
2025-08-22 14:24:49 +0300

    Fixed Bash installation script

--- launch_ollama.sh
+++ launch_ollama.sh
@@ -1,9 +1,12 @@
 #Installing Ollama
 curl -fsSL https://ollama.com/install.sh | sh
 
-#Downloading LLM
+#Downloading LLMs
+ollama pull gemma3:270m
 ollama pull gemma3:1b
 
+#Stopping Ollama
+sudo pkill -9 ollama
+
 #Setting environmental variables and starting Ollama
-OLLAMA_ORIGINS=chrome-extension://* 
-ollama serve
+OLLAMA_ORIGINS=chrome-extension://* ollama serve
